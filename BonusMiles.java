f12d26dbe205ccb24f205fe7325219390f1732c6
yamotherter <86082670+yamotherter@users.noreply.github.com>
2021-10-08 22:22:17 +0300

    Update BonusMiles.java

--- BonusMiles.java
+++ BonusMiles.java
@@ -1,13 +1,20 @@
 public class BonusMiles {
+    
     public static void main(String[] args) {
+        
 
         // Объявляем переменную стоимости билета и одной бонусной мили
+        
         // Присваиваем им значения
+        
         float ticket = 5765.8F;
+        
         int mile_cost = 20;
 
         // Рассчитываете количество бонусных миль
+        
         // И выводим значение в терминал
+        
         int bonus_miles = (int)ticket / mile_cost;
 
         System.out.print("Bonus miles: " + bonus_miles);
